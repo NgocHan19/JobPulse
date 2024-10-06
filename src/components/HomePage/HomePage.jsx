b9353368579c0eae70347ce86c59c0b79ecc7e88
tham <nlht1610@gmail.com>
2024-10-06 19:44:25 +0700

    THAM
    JP-6 <Code BE các chức năng chung>

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+export default HomePage;
