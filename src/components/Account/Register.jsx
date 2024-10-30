d6416809044fc2c20039fd8a3b0cc6e8989630ec
LuuCuong1912 <luulycuong070809@gmail.com>
2024-10-30 22:36:24 +0700

    JP-3 <FE: Save_Jobs, Search_Home_Logged, Search_Logged>

--- src/components/Account/Register.jsx
+++ src/components/Account/Register.jsx
@@ -75,14 +75,14 @@ const Register = () => {
 
   return (
     <div className="relative w-full h-[1080px] bg-white">
-      <h1 className="absolute left-[154px] top-[65px] text-[#1A73E8] font-bold text-[36px] leading-[44px]">
+      <h1 className="absolute left-[154px] top-[65px] text-[#1A73E8] font-bold text-4xl leading-[44px]">
         Chào mừng bạn đến với JobPulse
       </h1>
-      <p className="absolute left-[154px] top-[123px] text-[#A2A2A2] font-normal text-[28px] leading-[34px]">
+      <p className="absolute left-[154px] top-[123px] text-[#A2A2A2] font-normal text-xl leading-[34px]">
         Cùng xây dựng một hồ sơ nổi bật và nhận được các cơ hội sự nghiệp lý tưởng
       </p>
 
-      <label htmlFor="fullName" className="absolute left-[154px] top-[200px] text-[#868585] font-normal text-[24px] leading-[29px]">
+      <label htmlFor="fullName" className="absolute left-[154px] top-[200px] text-[#868585] font-normal text-2xl leading-[29px]">
         Họ và tên
       </label>
       <div className="absolute left-[154px] top-[240px] w-[827px] h-[60px] bg-white border border-[#A3A3A3] rounded-[5px] flex items-center">
@@ -97,7 +97,7 @@ const Register = () => {
         />
       </div>
 
-      <label htmlFor="email" className="absolute left-[154px] top-[340px] text-[#868585] font-normal text-[24px] leading-[29px]">
+      <label htmlFor="email" className="absolute left-[154px] top-[340px] text-[#868585] font-normal text-2xl leading-[29px]">
         Email
       </label>
       <div className="absolute left-[154px] top-[380px] w-[827px] h-[60px] bg-white border border-[#A3A3A3] rounded-[5px] flex items-center">
@@ -112,31 +112,31 @@ const Register = () => {
         />
       </div>
 
-      <label htmlFor="password" className="absolute left-[154px] top-[480px] text-[#868585] font-normal text-[24px] leading-[29px]">
+      <label htmlFor="password" className="absolute left-[154px] top-[480px] text-[#868585] font-normal text-2xl leading-[29px]">
         Mật khẩu
       </label>
       <div className="absolute left-[154px] top-[520px] w-[827px] h-[60px] bg-white border border-[#A3A3A3] rounded-[5px] flex items-center">
-        <img src={images['icon_password.png']} alt="Icon" className="w-[30px] h-[30px] ml-[15px]" />
+        <img src={images['icon_pass.png']} alt="Icon" className="w-[30px] h-[30px] ml-[15px]" />
         <input
           id="password"
           type="password"
           placeholder="Nhập mật khẩu..."
-          className="ml-[15px] placeholder-[#A2A2A2] text-[20px] leading-[24px] outline-none w-full pr-[45px]"
+          className="ml-[15px] placeholder-[#A2A2A2] text-lg leading-[24px] outline-none w-full pr-[45px]"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
 
-      <label htmlFor="confirmPassword" className="absolute left-[154px] top-[620px] text-[#868585] font-normal text-[24px] leading-[29px]">
+      <label htmlFor="confirmPassword" className="absolute left-[154px] top-[620px] text-[#868585] font-normal text-2xl leading-[29px]">
         Nhập lại mật khẩu
       </label>
       <div className="absolute left-[154px] top-[660px] w-[827px] h-[60px] bg-white border border-[#A3A3A3] rounded-[5px] flex items-center">
-        <img src={images['icon_password.png']} alt="Icon" className="w-[30px] h-[30px] ml-[15px]" />
+        <img src={images['icon_pass.png']} alt="Icon" className="w-[30px] h-[30px] ml-[15px]" />
         <input
           id="confirmPassword"
           type="password"
           placeholder="Nhập lại mật khẩu..."
-          className="ml-[15px] placeholder-[#A2A2A2] text-[20px] leading-[24px] outline-none w-full pr-[45px]"
+          className="ml-[15px] placeholder-[#A2A2A2] text-lg leading-[24px] outline-none w-full pr-[45px]"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
@@ -149,7 +149,7 @@ const Register = () => {
           onChange={handleCheckboxChange}
           className="w-[24px] h-[24px] mr-[10px]"
         />
-        <span className="text-[#868585] text-[20px] leading-[24px]">Tôi đồng ý với các điều khoản sử dụng</span>
+        <span className="text-[#868585] text-lg leading-[24px]">Tôi đồng ý với các điều khoản sử dụng</span>
       </div>
 
       <button
@@ -159,6 +159,15 @@ const Register = () => {
         Đăng ký
       </button>
       {errorMessage && <div className="absolute left-[154px] top-[900px] text-red-800">{errorMessage}</div>}
+
+
+      <button className="absolute left-[398px] top-[900px] text-[#A2A2A2] font-normal text-[20px] leading-[24px]" 
+        onClick={() => navigate('/login')} >Bạn đã có tài khoản? Đăng nhập ngay</button>
+      <p className="absolute left-[350px] top-[1000px] text-[#A2A2A2] font-normal text-[20px] leading-[24px]">Vui lòng gọi tới số 0123456789 (giờ hành chính).</p>
+      <p className="absolute left-[402px] top-[960px] text-[#5E5D5D] font-bold text-[20px] leading-[24px]">Bạn gặp khó khăn khi tạo tài khoản?</p>
+      
+      <div className="absolute left-[226px] top-[940px] w-[700px] h-0 border border-[#D9D9D9]" />
+
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -54,53 +54,60 @@ function Header() {
 
       {/* Form showJobApplication*/}
       {showJobApplication && (
-        <div className="absolute w-[250px] h-[250px] left-[350px] top-[70px] bg-white rounded-md shadow-lg mt-2 z-10">
+        <div className="absolute w-[250px] h-[250px] left-[350px] top-[70px] bg-white rounded-md shadow-lg mt-2 z-[9999]">
           <div className="absolute w-[250px] h-[250px] bg-white rounded-md">
-            <div className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[15px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[15px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/home-logged-in')} >
               <img src={images['icon_search_blue.png']} alt="Search Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Tìm kiếm</span>
-            </div>
+            </button>
             <div className="absolute w-[220px] h-0 border border-[#E3DEDE] top-[65px] left-[15px]" />
-            <div className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[75px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[75px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/applied-jobs')} >
               <img src={images['icon_bag_blue.png']} alt="Applied Jobs Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Việc làm đã ứng tuyển</span>
-            </div>
+            </button>
             <div className="absolute w-[220px] h-0 border border-[#E3DEDE] top-[125px] left-[15px]" />
-            <div className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[135px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[135px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/saved-jobs')} >
               <img src={images['icon_heart_blue.png']} alt="Saved Jobs Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Việc làm đã lưu</span>
-            </div>
+            </button>
             <div className="absolute w-[220px] h-0 border border-[#E3DEDE] top-[185px] left-[15px]" />
-            <div className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[195px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[220px] h-[40px] bg-[#F1F3F4] rounded-md top-[195px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/suitable-jobs')} >
               <img src={images['icon_fit_blue.png']} alt="Matching Jobs Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Việc làm phù hợp</span>
-            </div>
+            </button>
           </div>
         </div>
       )}
 
       {/* Form showCVManagement */}
       {showCVManagement && (
-        <div className="absolute w-[200px] h-[190px] left-[600px] top-[70px] bg-white rounded-md shadow-lg mt-2 z-10">
+        <div className="absolute w-[200px] h-[190px] left-[600px] top-[70px] bg-white rounded-md shadow-lg mt-2 z-[9999]">
           <div className="absolute w-[200px] h-[190px] bg-white rounded-md">
-            <div className="absolute w-[170px] h-[40px] bg-[#F1F3F4] rounded-md top-[15px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[170px] h-[40px] bg-[#F1F3F4] rounded-md top-[15px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/create-cv')} >
               <img src={images['icon_create_cv_blue.png']} alt="Upload CV Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Tạo CV</span>
-            </div>
+            </button>
 
             <div className="absolute w-[170px] h-0 border border-[#E3DEDE] top-[65px] left-[15px]" />
 
-            <div className="absolute w-[170px] h-[40px] bg-[#F1F3F4] rounded-md top-[75px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[170px] h-[40px] bg-[#F1F3F4] rounded-md top-[75px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/upload-cv')} >
               <img src={images['icon_upload_cv_blue.png']} alt="Manage CV Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Tải CV lên</span>
-            </div>
+            </button>
 
             <div className="absolute w-[170px] h-0 border border-[#E3DEDE] top-[125px] left-[15px]" />
 
-            <div className="absolute w-[170px] h-[40px] bg-[#F1F3F4] rounded-md top-[135px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[170px] h-[40px] bg-[#F1F3F4] rounded-md top-[135px] left-[15px] flex items-center px-2"
+                onClick={() => navigate('/cv-management')} >
               <img src={images['icon_manage_cv.png']} alt="Manage CV Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Quản lý CV</span>
-            </div>
+            </button>
 
           </div>
         </div>
@@ -108,19 +115,19 @@ function Header() {
 
       {/* Form showCompanyManagement */}
       {showCompanyManagement && (
-        <div className="absolute w-[220px] h-[130px] left-[800px] top-[70px] bg-white rounded-md shadow-lg mt-2 z-10">
+        <div className="absolute w-[220px] h-[130px] left-[800px] top-[70px] bg-white rounded-md shadow-lg mt-2 z-[9999]">
           <div className="absolute w-[220px] h-[130px] bg-white rounded-md">
-            <div className="absolute w-[190px] h-[40px] bg-[#F1F3F4] rounded-md top-[15px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[190px] h-[40px] bg-[#F1F3F4] rounded-md top-[15px] left-[15px] flex items-center px-2">
               <img src={images['icon_top_company.png']} alt="Manage CV Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Top Công ty</span>
-            </div>
+            </button>
 
             <div className="absolute w-[190px] h-0 border border-[#E3DEDE] top-[65px] left-[15px]" />
 
-            <div className="absolute w-[190px] h-[40px] bg-[#F1F3F4] rounded-md top-[75px] left-[15px] flex items-center px-2">
+            <button className="absolute w-[190px] h-[40px] bg-[#F1F3F4] rounded-md top-[75px] left-[15px] flex items-center px-2">
               <img src={images['icon_company.png']} alt="Manage CV Icon" className="w-[20px] h-[20px] mr-2" />
               <span className="font-normal text-[16px] leading-[19px] text-black">Danh sách công ty</span>
-            </div>
+            </button>
           </div>
         </div>
       )}
